c4d16dcbfe3a1874454d5bdf9b9d1fc62b5696f1
sycodes95 <sycodes95@gmail.com>
2023-11-16 00:41:00 -0800

    added email to social media bar instead

--- src/components/content/components/about.tsx
+++ src/components/content/components/about.tsx
@@ -34,7 +34,7 @@ export default function About () {
           <Icon className="text-black transition-all duration-500 text-opacity-80 group-hover:text-white " path={mdiDna} size={1.5} />
         </div>
       </SheetTrigger>
-      <SheetContent className="w-full max-h-screen overflow-y-auto text-lg text-white bg-black border-none bg-opacity-90 md:-translate-x-1/2 fe h-fit min-h-fit md:w-1/2 font-main md:left-1/2" side={'bottom'}>
+      <SheetContent className="flex flex-col w-full max-h-screen overflow-y-auto text-lg text-white bg-black border-none bg-opacity-90 md:-translate-x-1/2 h-fit min-h-fit md:w-1/2 font-main md:left-1/2" side={'bottom'}>
         <SheetHeader className="flex flex-col gap-4">
           <SheetTitle className="flex items-center justify-center w-full text-2xl">
             <Icon className="text-white duration-500 text-opacity-80" path={mdiDna} size={1.5} />
@@ -67,13 +67,13 @@ export default function About () {
                 <PsychologyIcon/>
                 <span className="text-lg font-bold">people i like</span>
               </div>
-              <div className="grid items-center w-full grid-cols-2 gap-4 p-4 pl-6 ml-3 text-lg border-l border-white lg:grid-cols-4">
+              <div className="grid items-center w-full grid-cols-2 gap-4 p-4 pl-6 ml-3 text-lg border-l border-white xl:grid-cols-4">
                 {
                 peopleWhoInspireMe.map((person) => (
-                  <div className="flex flex-col items-center gap-2">
-                    <img className="object-cover rounded-full w-14 h-14" src={person.src} />
+                  <div className="flex flex-col items-center gap-2 p-4 border border-white">
+                    <img className="object-cover w-20 h-20 rounded-full" src={person.src} />
                     <div className="flex flex-col items-center">
-                      <span >{person.name}</span>
+                      <span className="overflow-hidden whitespace-nowrap text-ellipsis">{person.name}</span>
                       <span className="text-sm font-bold">{person.occupation}</span>
                     </div>
                     
@@ -105,25 +105,21 @@ export default function About () {
                 <div className="flex flex-col gap-2">
                   <a href="https://open.spotify.com/playlist/2lko74v3Zr9nIHiPjybgnM?si=8ad539a55e894216" className="flex gap-2 text-lg hover:underline" target="_blank">
                     <span className="font-bold text-green-500">Spotify</span>
-                    <span>Basic Bitch</span> 
+                    <span>Basic</span> 
                   </a>
                 </div>
-                {/* <span className="text-lg font-main">
-                  I initially got into programming due to my realization of how useful it could be to learn this skill, especially because tech is where the future is headed.
-                  My journey started with telling myself that I will fully commit to learning this craft for 1 month to see if it's something I could 
-                  see myself continue doing and more importantly, to see if I even enjoyed it at all.
-                </span>
-
-                <span className="text-lg font-main">
-                  As I kept improving and developing my skills as a web developer, programming pretty much became a video game for me which felt more fulfilling and rewarding
-                  than playing actual video games and thus saw myself doing this professionally.
-                </span> */}
+                
               </div>
             </div>
 
-            <SheetClose className="p-2 mt-4 text-white border border-white rounded-2xl" asChild={true} >
-              <span>Close X</span>
-            </SheetClose>
+            <div className="flex items-center justify-center w-full">
+              <SheetClose className="p-2 mt-4 text-white border border-white cursor-pointer w-fit" asChild={true} >
+                <span>Close X</span>
+              </SheetClose>
+
+            </div>
+
+            
 
             
           </SheetDescription>
